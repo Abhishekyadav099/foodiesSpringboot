65d432692e220a8dfcb664d6ffae24946aef4379
Shivam Singh <147270647+shivams1164@users.noreply.github.com>
2025-11-29 15:01:49 +0000

    Increase multipart limits and add upload exception handler (413)

---
+++ src/main/java/in/abhishek/foodiesapi/config/FileUploadExceptionAdvice.java
@@ -0,0 +1,19 @@
+package in.abhishek.foodiesapi.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Map<String, String>> handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body(Map.of("error", "File size exceeds maximum allowed size"));
+    }
+}
